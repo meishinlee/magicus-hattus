74bf0586915037763872d549ffd97af034150e3c
Mei Shin Lee <53961253+meishinlee@users.noreply.github.com>
2022-08-12 21:18:50 -0400

    test on matlab

--- webapp.py
+++ webapp.py
@@ -1,8 +1,11 @@
-# import numpy as np
 import gradio as gr
+import matlab.engine
+
+eng = matlab.engine.start_matlab()
 
 def uploadedImage(image):
-    return "image"
+    image_with_hat = eng.place_hat(image)
+    return image_with_hat
 
 def uploadedVideo(video):
     return "video"
@@ -11,5 +14,21 @@ def uploadedVideo(video):
 with gr.Blocks() as demo:
     ##### Intro #####
     gr.Markdown("<h1><center>Turn yourself into a magician using this tool!</center></h1>")
+
+    ##### Upload Image/Video #####
+    with gr.Tabs():
+        with gr.TabItem("Upload Image (Recommended)"):
+            image_input = gr.Image()
+            image_output = gr.Image()
+            image_button = gr.Button("Submit")
+            image_button.style(rounded=True, border=False, full_width=True)
+          
+        with gr.TabItem("Upload Video"):
+            video_input = gr.Video()
+            video_output = gr.Video()
+            video_button = gr.Button("Submit")
+
+    image_button.click(uploadedImage, inputs=image_input, outputs=image_output)
+    video_button.click(uploadedVideo, inputs=video_input, outputs=video_output)
 
 demo.launch(share=True)
